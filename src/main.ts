27e2ac41dbb38efacec6e6fc057d2b7d7d9a6235
jeremy powell <jp5486@gmail.com>
2022-03-23 23:58:52 -0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
@@ -7,6 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Person API')
+    .setDescription('Kanmon take home assignment')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
